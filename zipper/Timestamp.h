4d72012becc0550e0fae8d25d17451dc4c4f12b1
bensanmorris <bensan.morris@googlemail.com>
2022-01-02 15:01:28 +0000

    Use now() as time not file's time

--- zipper/Timestamp.h
+++ zipper/Timestamp.h
@@ -35,33 +35,6 @@ Timestamp::Timestamp(const std::string& filepath)
     std::time_t now = std::time(nullptr);
     timestamp = *std::localtime(&now);
 #if defined(USE_WINDOWS)
-    //Implementation based on Ian Boyd's https://stackoverflow.com/questions/20370920/convert-current-time-from-windows-to-unix-timestamp-in-c-or-c
-    HANDLE hFile1;
-    FILETIME filetime;
-    hFile1 = CreateFile(filepath.c_str(), GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-
-    if (hFile1 == INVALID_HANDLE_VALUE)
-    {
-        return;
-    }
-
-    if (!GetFileTime(hFile1, &filetime, NULL, NULL))
-    {
-        return;
-    }
-    const int64_t UNIX_TIME_START = 0x019DB1DED53E8000; //January 1, 1970 (start of Unix epoch) in "ticks"
-    const int64_t TICKS_PER_SECOND = 10000000; //a tick is 100ns
-
-    //Copy the low and high parts of FILETIME into a LARGE_INTEGER
-    //This is so we can access the full 64-bits as an Int64 without causing an alignment fault
-    LARGE_INTEGER li;
-    li.LowPart = filetime.dwLowDateTime;
-    li.HighPart = filetime.dwHighDateTime;
-
-    //Convert ticks since 1/1/1970 into seconds
-    time_t time_s = (li.QuadPart - UNIX_TIME_START) / TICKS_PER_SECOND;
-
-    timestamp = *std::localtime(&time_s);
 
 #elif __linux__
     struct stat buf;
